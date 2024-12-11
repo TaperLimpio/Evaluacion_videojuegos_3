296b054fb5cfc462fd0e18a5e695155785ad4d74
TaperLimpio <167125666+TaperLimpio@users.noreply.github.com>
2024-12-10 21:20:25 -0300

    se importo más assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-<<<<<<< HEAD
-{   
-    private Animator animador;
-    [SerializeField] float velocidad;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        animador = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-=======
 {
     private Animator animator;
     [SerializeField] float velocidad;
@@ -24,16 +11,10 @@ public class PlayerMovement : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
->>>>>>> d021421aec7355024030f23fe8eea980d701e64c
     void Update()
     {
         float ejeX = Input.GetAxis("Horizontal");
         float ejeY = Input.GetAxis("Vertical");
-<<<<<<< HEAD
-        var vectorMovimiento = new Vector3(ejeX,0,ejeY).normalized * Time.deltaTime;
-        transform.Translate(vectorMovimiento * velocidad);
-
-=======
         // Al crear un Vector3() con .normalized lo que logramos es que el personaje no se mueva
         // más rápido al usar los ejes diagonales.
         var vectorMovimiento = new Vector3(ejeX, 0, ejeY).normalized * Time.deltaTime;
@@ -45,6 +26,5 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             animator.SetTrigger("saltar");
         }
->>>>>>> d021421aec7355024030f23fe8eea980d701e64c
     }
 }
